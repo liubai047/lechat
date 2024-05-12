eab4b5c26cc1602f286a5deff813e1892d2e1084
pengmingf <46414336+pengmingf@users.noreply.github.com>
2024-05-12 10:39:18 +0800

    Update send.py

--- send.py
+++ send.py
@@ -8,54 +8,6 @@ import requests
 from openai import OpenAI
 
 
-# 阿里云接口
-# "set DASHSCOPE_API_KEY=sk-6870bf6315904a358da8a0860c357c0d"
-def call_with_messages(text):
-    messages = [{'role': 'system', 'content': '帮忙回复女朋友消息的机器人。'},
-                {'role': 'user', 'content': "女朋友对我说：{}。我该怎么幽默风趣的回复她。只返回一句对她说的话".format(text)}]
-    gen = Generation()
-    response = gen.call(Generation.Models.qwen_v1, enable_search=True, messages=messages, seed=random.randint(1, 99999), result_format='message',
-                        # set the result is message format.
-                        )
-    if response.status_code == HTTPStatus.OK:
-        print(response)
-    else:
-        print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (response.request_id, response.status_code, response.code, response.message))
-    try:
-        if response.status_code == HTTPStatus.OK:
-            res_text = response.output.choices[0].message.content
-            my_list = re.findall(r'“([^“]*)”', res_text)
-            if len(my_list) > 0:
-                res_text = my_list[0]
-            else:
-                res_text = "我没想好怎么说"
-        else:
-            res_text = "我布吉岛该怎么说"
-    except Exception as e:
-        print(e)
-        res_text = "我出了点问题"
-    return res_text
-
-
-message = [{"role": "user", "content": "你是一个哄女朋友很厉害的人，能找各种机会逗她开心，你称呼她为静静，接下来你会收到一些日常聊天的话，请你用合适的措辞进行回复。"}]
-flag = False
-
-
-def qqPilot(text):
-    api_key = \
-        "ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmhjSEFpT2lKd1pYSnpiMjVoYkMxdGFXeHZjR1Z1WnlJc0ltTnlaV0YwWlY5aGRDSTZNVFk1TkRBNE9UTXpOU3dpYjNKbklqb2k2SVctNks2djVZV3M1WS00TC1XdGtPV0ZyT1dQdU9lN2hPZTdoeV9vaGI3b3JxX292NURva0tYbHJaRGxoYXpsajdndlZFVkhMZVd1b3VhSXQtYWNqZVdLb2VtRHFDX21pSkRwZzcza3VwSG1tYnJtbkkwdjVicVU1NVNvNUxxbjVaT0I1NkNVNVktUjVMaXQ1Yi1ETC1pX2tPaVFwZVc4Z09XUGtlZTdoQ0lzSW05M2JtVnlJam9pYldsc2IzQmxibWNpZlEuTkd5dDBVNjJXSmw4bUdSUnN1RDZNTW5mMktMM1ZNYm13MXFyTnNuVkdIWQ=="
-    try:
-        message.append({"role": "user", "content": text})
-        response = requests.post("https://api-qpilot.woa.com/v1/chat/completions", headers={'Content-Type': 'application/json', 'Authorization': "Bearer {}".format(api_key)},
-                                 json={"stream": False, "model": "gpt-4", "messages": message, })
-        res_text = response.json()["choices"][0]["message"]["content"]
-        message.append({"role": "assistant", "content": res_text})
-    except Exception as e:
-        print(e)
-        res_text = ""
-    return res_text
-
-
 deepseekMessage = [{"role": "system", "content": "你是一个哄女朋友很厉害的人，能找各种机会逗她开心，你称呼她为静静，接下来你会收到一些日常聊天的话，请你用合适的措辞进行回复。"}]
 
 
